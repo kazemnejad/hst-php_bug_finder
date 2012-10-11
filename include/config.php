e8f472fe0aeb6dfd9bca02962e25e27f59d9cf4f
IRinvisible <meshkat76@gmail.com>
2012-10-11 19:20:55 +0330

    inam az in age oomad begoo khers!

--- include/config.php
+++ include/config.php
@@ -1,5 +1,23 @@
 <?php
-	global $config = array();
-	global $config['db']['user'] = "root";
-	
+	global $_config =array();
+	global $_config['db']['host'] = "localhost";
+	global $_config['db']['user'] = "root";
+	global $_config['db']['pass'] = "asghar";
+	global $_config['db']['name'] = "hst";
+	global $_config['db']['lName'] = "";
+	global $_config['main']['host'] = "localHost"
+	global $_config['main']['sideName'] = "mainHst";
+	global $_config['main']['user'] = "admin";
+	global $_config['main']['pass'] = "123456";
+	global $_config['main']['lName'] = "";
+	global $_config['main']['debugMode'] = ture;
+	global $_config['log']['path'] = "C:\xampp\htdocs\hts\logs";
+	global $_config['ftp']['user'] = "ftp_root";
+	global $_config['ftp']['pass'] = "123456";
+	global $_config['ftp']['host'] = "";
+	global $_config['ftp']['port'] = '21';
+	global $_config['cookie']['domain'] = '';
+	global $_config['cookie']['path'] = '';
+	global $_config['crawler']['limitTime'] = 0;
+	global $_config['crawler']['7']
 ?>
