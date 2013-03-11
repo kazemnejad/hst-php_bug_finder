8bbbe2f548699fe0fbe1716e67e5f88f0b00fad6
MaKa <amirhosein@maka-lap.(none)>
2013-03-11 17:44:15 +0330

    First

--- include/config.php
+++ include/config.php
@@ -1,31 +1,41 @@
 <?php
-	global $_config =array();
-	global $_config['db']['host'] = "localhost";
-	global $_config['db']['user'] = "root";
-	global $_config['db']['pass'] = "asghar";
-	global $_config['db']['name'] = "hst";
-	global $_config['db']['lName'] = "";
-	global $_config['main']['host'] = "localHost"
-	global $_config['main']['sideName'] = "mainHst";
-	global $_config['main']['user'] = "admin";
-	global $_config['main']['pass'] = "123456";
-	global $_config['main']['lName'] = "";
-	global $_config['main']['debugMode'] = ture;
-	global $_config['log']['path'] = "C:\xampp\htdocs\hts\logs";
-	global $_config['ftp']['user'] = "ftp_root";
-	global $_config['ftp']['pass'] = "123456";
-	global $_config['ftp']['host'] = "";
-	global $_config['ftp']['port'] = '21';
-	global $_config['cookie']['domain'] = '';
-	global $_config['crawler']['limitTime'] = 0;
-	global $_config['crawler']['locale'] = 'fa_IR';
-	global $_config['crawler']['max_depth'] = 3;
-	global $_config['crawler']['key_disable_key'] = false;
-	global $_config['crawler']['max_url_len'] = 1024;
-	global $_config['crawler']['allow_ext'] = array('html', 'txt', 'htm', 'xhtml', 'php', 'asp');;
-	global $_config['crawler']['max_url_content'] = 512*1024;
-	global $_config['crawler']['thread_sleep_time'] = 100000;
-	global $_config['crawler']['char_per_word'] = 3;
-	
-	
-?>
+error_reporting(E_ALL);
+ini_set("display_errors", "On");
+//global $_config;
+$_config = array();
+$_config['db']['host'] = "localhost";
+$_config['db']['user'] = "root";
+$_config['db']['pass'] = "";
+$_config['db']['name'] = "phpcrawl";
+$_config['db']['lName'] = "";
+$_config['main']['host'] = "localHost";
+$_config['main']['sideName'] = "mainHst";
+$_config['main']['user'] = "admin";
+$_config['main']['pass'] = "asdfghj";
+$_config['main']['lName'] = "";
+$_config['main']['debugMode'] = 1;
+$_config['main']['writeQueries'] = 1;
+$_config['log']['path'] = "C:/xampp/htdocs/logs";
+$_config['ftp']['user'] = "ftp_root";
+$_config['ftp']['pass'] = "";
+$_config['ftp']['host'] = "";
+$_config['ftp']['port'] = "21";
+$_config['cookie']['domain'] = '';
+$_config['crawler']['limitTime'] = 0;
+$_config['crawler']['locale'] = 'fa_IR';
+$_config['crawler']['max_depth'] = 3;
+$_config['crawler']['key_disable_key'] = false;
+$_config['crawler']['max_url_len'] = 1024;
+$_config['crawler']['allow_ext'] = array('html', 'txt', 'htm', 'xhtml', 'php', 'asp');;
+$_config['crawler']['max_url_content'] = 512*1024;
+$_config['crawler']['thread_sleep_time'] = 100000;
+$_config['crawler']['char_per_word'] = 3;
+$_config['parser']['last_page_parsed'] = 0;
+$_config['log']['SiteInfo'] = 0;
+
+function getConfig($componet, $value){
+	global $_config;
+	return @$_config[$componet][$value];
+}
+
+//echo getConfig('main', 'debugMode');
--- index.php
+++ index.php
@@ -1,12 +1,22 @@
-<html>
-	<head>
-		
-	</head>
-	<body>
-		<form action="crawler.php" method="GET">
-			URL: <input type="text" name="url" /><br>
-			forms<input type="checkbox" name="form" value="checked" /><br>
-			<input type="submit" name="submit" />
-		</form>
-	</body>
-</html>
+<?php
+require_once "template/header.php"
+?>
+<body onload="load()" >
+<form action="basic_info.php" method="get">
+	<div class="page_mid search_bar">
+		<input type="text"  id="url" name="url" value="" onfocus="f(this);" onblur="b(this);" class="search_field" />
+		<input type="submit" id="submit" name="submit" value="<?php doLan("Go", "fa");?>" class="submit" />
+	</div>
+</form>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+</body>
+
+<?php
+require_once "template/footer.php"
+?>
